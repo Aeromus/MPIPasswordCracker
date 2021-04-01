a0880c22eec63e64415afe2990ad2d3d6ce1a928
Aeromus <AMKnoblach@gmail.com>
2021-04-01 15:44:19 -0700

    Added read settings

--- passwordcracker.c
+++ passwordcracker.c
@@ -18,8 +18,36 @@ int main(int argc, char **argv) {
 	MPI_Comm_rank(MCW, &rank);
 	MPI_Status mystatus;
 	srand(rank);
-
+	int passwordLength = -1;
+	std::string hash = "";
+	std::string yes = "Y";
 	//Get settings in rank 0 then distribute settings to other ranks
+	if(rank == 0){
+		//Settings 
+		// -Known password length
+		// -Provide password hash
+		// 
+
+		std::string knownLength = "";
+		std::cout << "\nIs the length of the password known? (Y/n) ";
+		std::getline(std::cin, knownLength);
+		if(yes.compare(knownLength) == 0){
+			std::cout<< "\nHow long is the password?  " ;
+			std::cin >> passwordLength;
+
+			//flush buffer
+			std::string s;
+			std::getline(std::cin, s);
+
+		}
+
+		std::cout << "\nWhat is the password hash? ";
+		std::getline(std::cin, hash);
+
+		std::cout<< "The password hash is " << hash <<std::endl;
+		std::cout<< "The lenght of the password is " << passwordLength << std::endl;
+	}
+
 
 	//Calculate password space
 
