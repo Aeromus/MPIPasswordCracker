7da959a7dfef40822f76d74e46b9ec9553a63d19
Aeromus <AMKnoblach@gmail.com>
2021-04-01 11:15:24 -0700

    Added sudo code for reals this time

--- passwordcracker.c
+++ passwordcracker.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
+#include <string>
+
 
 #define MCW MPI_COMM_WORLD
 
@@ -10,14 +12,43 @@ int main(int argc, char **argv) {
 
 	int rank, size, data;
 
+	//MPI INIT Stuff
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MCW, &size);
 	MPI_Comm_rank(MCW, &rank);
+	MPI_Status mystatus;
+	srand(rank);
 
-	MPI_Send(&rank, 1, MPI_INT, (rank+1)%size, 0, MCW);
-	MPI_Recv(&data, 1, MPI_INT, MPI_ANY_SOURCE, 0, MCW, MPI_STATUS_IGNORE);
-	std::cout<<"I am "<<rank<<" of "<<size<<"; got a message from "<<data<<"\n";
+	//Get settings in rank 0 then distribute settings to other ranks
 
+	//Calculate password space
+
+
+
+	//Get input hash
+
+	//Generate password space a-z A-Z 0-9
+
+	//Loop till password found
+
+	//Check current generated string
+
+	//if password == string break
+
+	//iterate to next string
+
+	//if no next string look for work from other processes
+
+
+
+
+
+
+
+	//Once password found report string and hash
+
+
+	//Used to prevent stack smashing & Desync issues
 	MPI_Finalize();
 
 	return 0;
